3644bd88855a3d0e6ac8eb14fcb425c542d5141b
danquan <dangquanattvhouse@gmail.com>
2025-11-20 21:20:48 +0700

    add template geometry

---
+++ code/real-geometry.cc
@@ -0,0 +1,245 @@
+
+template <class T> int sgn(T x) { return (x > 0) - (x < 0); }
+template<class T>
+struct Point {
+	typedef Point P;
+	T x, y;
+	explicit Point(T x=0, T y=0) : x(x), y(y) {}
+	bool operator<(P p) const { return tie(x,y) < tie(p.x,p.y); }
+	bool operator==(P p) const { return tie(x,y)==tie(p.x,p.y); }
+	P operator+(P p) const { return P(x+p.x, y+p.y); }
+	P operator-(P p) const { return P(x-p.x, y-p.y); }
+	P operator*(T d) const { return P(x*d, y*d); }
+	P operator/(T d) const { return P(x/d, y/d); }
+	T dot(P p) const { return x*p.x + y*p.y; }
+	T cross(P p) const { return x*p.y - y*p.x; }
+	T cross(P a, P b) const { return (a-*this).cross(b-*this); }
+	T dist2() const { return x*x + y*y; }
+	double dist() const { return sqrt((double)dist2()); }
+	// angle to x-axis in interval [-pi, pi]
+	double angle() const { return atan2(y, x); }
+	P unit() const { return *this/dist(); } // makes dist()=1
+	P perp() const { return P(-y, x); } // rotates +90 degrees
+	P normal() const { return perp().unit(); }
+	// returns point rotated 'a' radians ccw around the origin
+	P rotate(double a) const {
+		return P(x*cos(a)-y*sin(a),x*sin(a)+y*cos(a)); }
+	friend ostream& operator<<(ostream& os, P p) {
+		return os << "(" << p.x << "," << p.y << ")"; }
+};
+
+/* On Segment: p is on segment from s to e */
+template<class P> bool onSegment(P s, P e, P p) {
+	return p.cross(s, e) == 0 && (s - p).dot(e - p) <= 0;
+}
+
+/* Segment Intersection */
+template<class P> vector<P> segInter(P a, P b, P c, P d) {
+	auto oa = c.cross(d, a), ob = c.cross(d, b),
+	     oc = a.cross(b, c), od = a.cross(b, d);
+	// Checks if intersection is single non-endpoint point.
+	if (sgn(oa) * sgn(ob) < 0 && sgn(oc) * sgn(od) < 0)
+		return {(a * ob - b * oa) / (ob - oa)};
+	set<P> s;
+	if (onSegment(c, d, a)) s.insert(a);
+	if (onSegment(c, d, b)) s.insert(b);
+	if (onSegment(a, b, c)) s.insert(c);
+	if (onSegment(a, b, d)) s.insert(d);
+	return {all(s)};
+}
+
+/**
+ * Apply the linear transformation (translation, rotation and scaling) which takes line p0-p1 to line q0-q1 to point r.
+ */
+typedef Point<double> P;
+P linearTransformation(const P& p0, const P& p1,
+		const P& q0, const P& q1, const P& r) {
+	P dp = p1-p0, dq = q1-q0, num(dp.cross(dq), dp.dot(dq));
+	return q0 + P((r-p0).cross(num), (r-p0).dot(num))/dp.dist2();
+}
+
+/**
+ * Computes the pair of points at which two circles intersect.
+ */
+typedef Point<double> P;
+bool circleInter(P a,P b,double r1,double r2,pair<P, P>* out) {
+	if (a == b) { assert(r1 != r2); return false; }
+	P vec = b - a;
+	double d2 = vec.dist2(), sum = r1+r2, dif = r1-r2,
+	       p = (d2 + r1*r1 - r2*r2)/(d2*2), h2 = r1*r1 - p*p*d2;
+	if (sum*sum < d2 || dif*dif > d2) return false;
+	P mid = a + vec*p, per = vec.perp() * sqrt(fmax(0, h2) / d2);
+	*out = {mid + per, mid - per};
+	return true;
+}
+
+/**
+ * Finds the tangents of two circles, external if r2 > 0, internal if r2 < 0.
+ */
+template<class P>
+vector<pair<P, P>> tangents(P c1, double r1, P c2, double r2) {
+	P d = c2 - c1;
+	double dr = r1 - r2, d2 = d.dist2(), h2 = d2 - dr * dr;
+	if (d2 == 0 || h2 < 0)  return {};
+	vector<pair<P, P>> out;
+	for (double sign : {-1, 1}) {
+		P v = (d * dr + d.perp() * sqrt(h2) * sign) / d2;
+		out.push_back({c1 + v * r1, c2 + v * r2});
+	}
+	if (h2 == 0) out.pop_back();
+	return out;
+}
+
+/**
+ * Returns the area of the intersection of a circle with a ccw polygon.
+ */
+typedef Point<double> P;
+#define arg(p, q) atan2(p.cross(q), p.dot(q))
+double circlePoly(P c, double r, vector<P> ps) {
+	auto tri = [&](P p, P q) {
+		auto r2 = r * r / 2;
+		P d = q - p;
+		auto a = d.dot(p)/d.dist2(), b = (p.dist2()-r*r)/d.dist2();
+		auto det = a * a - b;
+		if (det <= 0) return arg(p, q) * r2;
+		auto s = max(0., -a-sqrt(det)), t = min(1., -a+sqrt(det));
+		if (t < 0 || 1 <= s) return arg(p, q) * r2;
+		P u = p + d * s, v = q + d * (t-1);
+		return arg(p,u) * r2 + u.cross(v)/2 + arg(v,q) * r2;
+	};
+	auto sum = 0.0;
+	rep(i,0,sz(ps))
+		sum += tri(ps[i] - c, ps[(i + 1) % sz(ps)] - c);
+	return sum;
+}
+/**
+ * The circumcirle of a triangle is the circle intersecting all three vertices.
+ *  ccRadius returns the radius of the circle going through points A, B and C and ccCenter returns the center of the same circle.
+ */
+typedef Point<double> P;
+double ccRadius(const P& A, const P& B, const P& C) {
+	return (B-A).dist()*(C-B).dist()*(A-C).dist()/
+			abs((B-A).cross(C-A))/2;
+}
+P ccCenter(const P& A, const P& B, const P& C) {
+	P b = C-A, c = B-A;
+	return A + (b*c.dist2()-c*b.dist2()).perp()/b.cross(c)/2;
+}
+
+/**
+ * Computes the minimum circle that encloses a set of points.
+ */
+typedef Point<double> P;
+pair<P, double> mec(vector<P> ps) {
+	shuffle(all(ps), mt19937(time(0)));
+	P o = ps[0];
+	double r = 0, EPS = 1 + 1e-8;
+	rep(i,0,sz(ps)) if ((o - ps[i]).dist() > r * EPS) {
+		o = ps[i], r = 0;
+		rep(j,0,i) if ((o - ps[j]).dist() > r * EPS) {
+			o = (ps[i] + ps[j]) / 2;
+			r = (o - ps[i]).dist();
+			rep(k,0,j) if ((o - ps[k]).dist() > r * EPS) {
+				o = ccCenter(ps[i], ps[j], ps[k]);
+				r = (o - ps[i]).dist();
+			}
+		}
+	}
+	return {o, r};
+}
+
+/**
+ * Returns true if p lies within the polygon. 
+ * If strict is true, it returns false for points on the boundary. 
+ * The algorithm uses products in intermediate steps so watch out for overflow.
+ */
+template<class P>
+bool inPolygon(vector<P> &p, P a, bool strict = true) {
+	int cnt = 0, n = sz(p);
+	rep(i,0,n) {
+		P q = p[(i + 1) % n];
+		if (onSegment(p[i], q, a)) return !strict;
+		//or: if (segDist(p[i], q, a) <= eps) return !strict;
+		cnt ^= ((a.y<p[i].y) - (a.y<q.y)) * a.cross(p[i], q) > 0;
+	}
+	return cnt;
+}
+
+/**
+ * Returns the center of mass for a polygon.
+ */
+typedef Point<double> P;
+P polygonCenter(const vector<P>& v) {
+	P res(0, 0); double A = 0;
+	for (int i = 0, j = sz(v) - 1; i < sz(v); j = i++) {
+		res = res + (v[i] + v[j]) * v[j].cross(v[i]);
+		A += v[j].cross(v[i]);
+	}
+    return res / A / 3;
+}
+
+/**
+ * Returns a vector with the vertices of a polygon with everything to the left of the vector going from s to e cut away.
+ */
+typedef Point<double> P;
+vector<P> polygonCut(const vector<P>& poly, P s, P e) {
+	vector<P> res;
+	rep(i,0,sz(poly)) {
+		P cur = poly[i], prev = i ? poly[i-1] : poly.back();
+		auto a = s.cross(e, cur), b = s.cross(e, prev);
+		if ((a < 0) != (b < 0))
+			res.push_back(cur + (prev - cur) * (a / (a - b)));
+		if (a < 0)
+			res.push_back(cur);
+	}
+	return res;
+}
+
+/**
+ * Line-convex polygon intersection. The polygon must be ccw and have no collinear points. lineHull(line, poly) returns a pair describing the intersection of a line with the polygon:
+ *  \begin{itemize*}
+ *    \item $(-1, -1)$ if no collision,
+ *    \item $(i, -1)$ if touching the corner $i$,
+ *    \item $(i, i)$ if along side $(i, i+1)$,
+ *    \item $(i, j)$ if crossing sides $(i, i+1)$ and $(j, j+1)$.
+ *  \end{itemize*}
+ */
+#define cmp(i,j) sgn(dir.perp().cross(poly[(i)%n]-poly[(j)%n]))
+#define extr(i) cmp(i + 1, i) >= 0 && cmp(i, i - 1 + n) < 0
+template <class P> int extrVertex(vector<P>& poly, P dir) {
+	int n = sz(poly), lo = 0, hi = n;
+	if (extr(0)) return 0;
+	while (lo + 1 < hi) {
+		int m = (lo + hi) / 2;
+		if (extr(m)) return m;
+		int ls = cmp(lo + 1, lo), ms = cmp(m + 1, m);
+		(ls < ms || (ls == ms && ls == cmp(lo, m)) ? hi : lo) = m;
+	}
+	return lo;
+}
+
+#define cmpL(i) sgn(a.cross(poly[i], b))
+template <class P>
+array<int, 2> lineHull(P a, P b, vector<P>& poly) {
+	int endA = extrVertex(poly, (a - b).perp());
+	int endB = extrVertex(poly, (b - a).perp());
+	if (cmpL(endA) < 0 || cmpL(endB) > 0)
+		return {-1, -1};
+	array<int, 2> res;
+	rep(i,0,2) {
+		int lo = endB, hi = endA, n = sz(poly);
+		while ((lo + 1) % n != hi) {
+			int m = ((lo + hi + (lo < hi ? 0 : n)) / 2) % n;
+			(cmpL(m) == cmpL(endB) ? lo : hi) = m;
+		}
+		res[i] = (lo + !cmpL(hi)) % n;
+		swap(endA, endB);
+	}
+	if (res[0] == res[1]) return {res[0], -1};
+	if (!cmpL(res[0]) && !cmpL(res[1]))
+		switch ((res[0] - res[1] + sz(poly) + 1) % sz(poly)) {
+			case 0: return {res[0], res[0]};
+			case 2: return {res[1], res[1]};
+		}
+	return res;
+}
